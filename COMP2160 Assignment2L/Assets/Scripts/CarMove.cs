e7765c1177c06f0ae91e787622b48266a2f3399a
Liting-Mao <60677197+Liting-Mao@users.noreply.github.com>
2020-11-08 11:50:28 +0800

    CarMove

---
+++ COMP2160 Assignment2L/Assets/Scripts/CarMove.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    public Rigidbody theRB;
+
+    public float forwardAccel = 0.1f;
+    public float reverseAccel = 0.1f;
+    public float maxSpeed = 50f;
+    public float turnStrength = 180f;
+    public float gravityForce = 10f;
+    public float dragOnGround = 3f;
+
+
+    float speedInput, turnInput;
+
+    bool grounded;
+    public LayerMask whatIsGround;
+    public float groundRayLength = 0.5f;
+    public Transform groundRayPoint;
+
+    void Start()
+    {
+
+        theRB.transform.parent = null;
+    }
+    private void Update()
+    {
+        speedInput = 0f;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * forwardAccel * 1000f;
+
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            speedInput = Input.GetAxis("Vertical") * reverseAccel * 1000f;
+
+        }
+
+        turnInput = Input.GetAxis("Horizontal");
+
+        if (grounded)
+        {
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, turnInput * turnStrength * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+
+        }
+
+        transform.position = theRB.transform.position;
+    }
+    private void FixedUpdate()
+    {
+        grounded = false;
+        RaycastHit hit;
+        if (Physics.Raycast(groundRayPoint.position, -transform.up, out hit, groundRayLength, whatIsGround))
+        {
+
+            grounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        if (grounded)
+        {
+            theRB.drag = dragOnGround;
+
+            if (Mathf.Abs(speedInput) > 0)
+            {
+                theRB.AddForce(transform.forward * speedInput);
+
+            }
+        }
+        else
+        {
+            theRB.drag = 0.1f;
+
+            theRB.AddForce(Vector3.up * -gravityForce * 100f);
+        }
+
+    }
+}
