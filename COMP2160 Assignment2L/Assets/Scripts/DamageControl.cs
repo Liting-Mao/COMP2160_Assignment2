6a9a991683d51208c00e136db11aee52e413a7dc
44423551yuyilin <yuyi.lin@students.mq.edu.au>
2020-11-08 14:19:44 +0800

    HealthBar and TreeDamage

---
+++ COMP2160 Assignment2L/Assets/Scripts/DamageControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageControl : MonoBehaviour
+{
+    public HealthBar healthbar;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Tree")
+        {
+            if (healthbar)
+            {
+                healthbar.onTakeDamage(20);
+            }
+        }
+    }
+}
